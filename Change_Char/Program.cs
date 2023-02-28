549d52994d8d3ffa75c95fda09e8462acd829e6b
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-03-01 00:26:24 +0300

    Add files via upload

---
+++ Change_Char/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Change_Char;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your string: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(" ");
+        List<string> newWords = new List<string>();
+
+        for(int i = 0; i < words.Length; i++)
+        {
+            char firstChar = words[i][0];
+            char lastChar = words[i][words[i].Length - 1];
+
+            string newWord = lastChar + words[i].Substring(1, words[i].Length - 2) + firstChar;
+            newWords.Add(newWord);
+        }
+
+        foreach (var item in newWords)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
