decedb1dcc81d87dd78a5c3ec91631e59abe4538
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-03-01 00:44:45 +0300

    Add files via upload

---
+++ Consonant/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Consonant;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your string: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(" ");
+        string consonants = "bcdfghjklmnpqrstvwxyz";
+        
+        foreach (string word in words)
+        {
+            bool hasConsonants = false;
+
+            for(int i = 0; i < word.Length-1 ; i++)
+            {
+                if(consonants.Contains(word[i]) && consonants.Contains(word[i+1]))
+                {
+                    hasConsonants = true;
+                    break;
+                }
+            }
+
+            Console.Write(hasConsonants + " ");
+        }
+    }
+}
